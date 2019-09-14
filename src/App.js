25fcb651cdceca5b6664323b3d4eb31f78bf59a6
Adam Carson <adam.carson@lvlsvn.com>
2019-09-14 10:58:52 -0400

    Filtering for land success and some styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import DataTable from './components/DataTable';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container App">
       <div className="row">
         <div className="col-12">
+          <h1>SpaceX Launches</h1>
           <DataTable />
         </div>
       </div>
--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -14,10 +14,11 @@ class DataTable extends React.Component{
 			data: [],
 			isLoading: true,
 			filters: {
-				land_success: true,
-				reuse: false,
-				with_reddit: false
-			}
+				land_success: false
+			},
+			filtered: [{
+				id: "land_success", value: ""
+			}]
 		}
 
 		this.getLaunches();
@@ -28,6 +29,16 @@ class DataTable extends React.Component{
 		this.setState({data: response.data, isLoading: false});
 	}
 
+	handleLandSuccess(e){
+		let filtered = this.state.filtered;
+		let filters = this.state.filters;
+
+		filters.land_success = !filters.land_success
+		filtered[0].value = filters.land_success ? "true" : "";
+
+		this.setState({filters: filters, filtered: filtered})
+	}
+
 	renderTable(){
 		if(this.state.isLoading){
 			return 'Loading Data';
@@ -36,38 +47,70 @@ class DataTable extends React.Component{
 				<ReactTable
 					data={this.state.data}
 					filterable
+					filtered={this.state.filtered}
+        	onFilteredChange={filtered => this.setState({ filtered })}
 					columns={[
 						{
 							Header: "Badge",
 							accessor: "links.mission_patch_small",
-							Cell: ({value}) => (<img src={value} />)
+							Cell: ({value}) => (<img src={value} />),
+							filterable: false
 						},
 						{
 							Header: "Rocket Name",
-							accessor: "rocket.rocket_name"
+							accessor: "rocket.rocket_name",
+							filterable: false
 						},
 						{
 							Header: "Rocket Type",
-							accessor: "rocket.rocket_type"
+							accessor: "rocket.rocket_type",
+							filterable: false
 						},
 						{
 							Header: "Launch Date",
 							accessor: "launch_date_utc",
-							Cell: ({value}) => (Moment(value).format("L"))
+							Cell: ({value}) => (Moment(value).format("L")),
+							filterable: false
 						},
 						{
 							Header: "Details",
-							accessor: "details"
+							accessor: "details",
+							filterable: false
 						},
 						{
 							Header: "ID",
-							accessor: "flight_number"
+							accessor: "flight_number",
+							filterable: false
 						},
 						{
 							Header: "Article",
 							accessor: "links.article_link",
-							Cell: ({value}) => (<a href={value} target="_blank"><img src={Link} /></a>)
-						}
+							Cell: ({value}) => (<a href={value} target="_blank"><img src={Link} /></a>),
+							filterable: false
+						},
+						{
+							accessor: (row) => {
+								let success = 'false';
+								if(row.rocket.first_stage.cores.length > 0){
+									for(let e of row.rocket.first_stage.cores){
+										if(e.land_success){
+											success = 'true';
+											break;
+										}
+									}
+								}
+								return success;
+							},
+							id: "land_success",
+							show: false,
+							filterMethod: (filter, row) => {
+								if(filter.value){
+									return row[filter.id] === "true";
+								}else{
+									return true;
+								}
+              }
+            }
 					]}
 				/>
 			);
@@ -78,7 +121,21 @@ class DataTable extends React.Component{
 		
 		console.log(this.state.data);
 		return(
-			<div>{this.renderTable()}</div>
+			
+
+			<div>
+				<div>
+					<label>
+						          Land Success
+						          <input
+						            name="landSuccess"
+						            type="checkbox"
+						            checked={this.state.filters.land_success}
+						            onChange={event => {this.handleLandSuccess()}} />
+						        </label>
+				</div>
+				{this.renderTable()}
+			</div>
 		);
 	}
 }
